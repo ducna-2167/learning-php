91aa55f5db84f84bdef783467495544d8def1210
nguyen.anh.duc <nguyen.anh.duc@B122167-MAC.local>
2022-05-31 17:33:24 +0700

    add new file

--- phpproject/includes/classes/Database.php
+++ phpproject/includes/classes/Database.php
@@ -1,21 +1,38 @@
 <?php
 
-class Database{        
+class Database
+{
 
     const SELECTSINGLE = 1;
     const SELECTALL = 2;
     const EXECUTE = 3;
-        
+
     private $pdo;
 
-    public function __construct(){
-        
-        $pdo = new PDO("mysql:host=localhost;dbname=project", "project_admin", "Your Password");
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        
+    public function __construct()
+    {
+
+        $this->pdo = new PDO("mysql:host=localhost;dbname=project", "project_admin", "password");
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    //Add queryDB() here
-    
+    public function queryDB($sql, $mode, $values = array())
+    {
+        # code...
+        $stmt = $this->pdo->prepare($sql);
+
+        foreach ($values as $valueToBind) {
+            $stmt->bindValue($valueToBind[0], $valueToBind[1]);
+        }
+
+        $stmt->execute();
+
+        if ($mode != Database::SELECTSINGLE && $mode != Database::SELECTALL && $mode != Database::EXECUTE) {
+            throw new Exception('Invalid Mode');
+        } else if ($mode == Database::SELECTSINGLE) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else if ($mode == Database::SELECTALL) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
 }
-    
