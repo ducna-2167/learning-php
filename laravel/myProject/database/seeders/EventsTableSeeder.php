2286e76e4bc1682f293eeb3124947d178ced128d
nguyen.anh.duc <nguyen.anh.duc@B122167-MAC.local>
2022-06-15 16:59:48 +0700

    add seeder

---
+++ laravel/myProject/database/seeders/EventsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Event;
+use Illuminate\Support\Facades\DB;
+
+class EventsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('events')->truncate();
+
+        $faker = \Faker\Factory::create();
+        foreach (range(1, 50) as $index) {
+            Event::create([
+                'name' => $faker->sentence(2),
+                'city' => $faker->city,
+                'venue' => $faker->company,
+                'description' => $faker->paragraphs(1, true),
+            ]);
+        }
+    }
+}
